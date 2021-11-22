9ebeb166314d643b6632cb9ea300ff932ed330ce
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-22 16:50:42 +0100

    add nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { BsChevronLeft } from 'react-icons/bs';
+import '../sectionStyles/nav.css';
+
+const Navbar = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  return (
+    <nav>
+      <div className="location-path">
+        {(location.pathname !== '/') ? <button className="back" type="button" onClick={history.goBack}><BsChevronLeft className="fas fa-chevron-left"> </BsChevronLeft></button> : <span className="back">Countries</span>}
+        {' '}
+        <span className="location">
+          {location.pathname}
+        </span>
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
