3ffe8f67bc9aeef07be6c949b4f77114912e2b6f
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-23 13:07:56 +0100

    add african countries

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import { Browser, router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
+import App from 'App';
+import store from './redux/configureStore';
+import { loadDataThunk } from './redux/covid/covid';
+import './styles/index.css';
+
+store.dispatch(loadDataThunk());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
